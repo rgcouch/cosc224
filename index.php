e1c25f69297798aca3f015ee6bb857f5eecb6a5b
Calvin <calvintourand@gmail.com>
2017-01-25 12:11:50 -0800

    vwts code

---
+++ index.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" lang="en-US">
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" lang="en-US">
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" lang="en-US">
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html lang="en-US">
+<!--<![endif]-->
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width" />
+<title>Transition House | Vernon Women&#039;s Transition House Society</title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="http://vwts.ca/wp-content/themes/transition-house/style.css" />
+<link rel="pingback" href="http://vwts.ca/xmlrpc.php" />
+<!--[if lt IE 9]>
+<script src="http://vwts.ca/wp-content/themes/transition-house/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+<link rel="alternate" type="application/rss+xml" title="Transition House &raquo; Feed" href="http://vwts.ca/feed/" />
+<link rel="alternate" type="application/rss+xml" title="Transition House &raquo; Comments Feed" href="http://vwts.ca/comments/feed/" />
+<link rel="alternate" type="application/rss+xml" title="Transition House &raquo; Home Comments Feed" href="http://vwts.ca/home/feed/" />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"http:\/\/s.w.org\/images\/core\/emoji\/72x72\/","ext":".png","source":{"concatemoji":"http:\/\/vwts.ca\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.2.11"}};
+			!function(a,b,c){function d(a){var c=b.createElement("canvas"),d=c.getContext&&c.getContext("2d");return d&&d.fillText?(d.textBaseline="top",d.font="600 32px Arial","flag"===a?(d.fillText(String.fromCharCode(55356,56812,55356,56807),0,0),c.toDataURL().length>3e3):(d.fillText(String.fromCharCode(55357,56835),0,0),0!==d.getImageData(16,16,1,1).data[0])):!1}function e(a){var c=b.createElement("script");c.src=a,c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g;c.supports={simple:d("simple"),flag:d("flag")},c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.simple&&c.supports.flag||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link rel='stylesheet' id='cptch_stylesheet-css'  href='http://vwts.ca/wp-content/plugins/captcha/css/style.css?ver=4.2.11' type='text/css' media='all' />
+<link rel='stylesheet' id='jquery-ui-css'  href='http://vwts.ca/wp-content/plugins/contact-form-builder/css/jquery-ui-1.10.3.custom.css?ver=4.2.11' type='text/css' media='all' />
+<link rel='stylesheet' id='contact_form_maker_frontend-css'  href='http://vwts.ca/wp-content/plugins/contact-form-builder/css/contact_form_maker_frontend.css?ver=4.2.11' type='text/css' media='all' />
+<script type='text/javascript' src='http://vwts.ca/wp-includes/js/jquery/jquery.js?ver=1.11.2'></script>
+<script type='text/javascript' src='http://vwts.ca/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<script type='text/javascript' src='https://maps.google.com/maps/api/js?sensor=false&#038;ver=4.2.11'></script>
+<script type='text/javascript' src='http://vwts.ca/wp-content/plugins/contact-form-builder/js/if_gmap_front_end.js?ver=4.2.11'></script>
+<script type='text/javascript' src='http://vwts.ca/wp-content/plugins/contact-form-builder/js/cfm_main_front_end.js?ver=1.0.18'></script>
+<script type='text/javascript' src='http://vwts.ca/wp-content/plugins/vslider/js/vslider.js?ver=4.2.11'></script>
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://vwts.ca/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://vwts.ca/wp-includes/wlwmanifest.xml" /> 
+<meta name="generator" content="WordPress 4.2.11" />
+<link rel='canonical' href='http://vwts.ca/' />
+<link rel='shortlink' href='http://vwts.ca/' />
+	<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+</head>
+
+<body class="home page page-id-103 page-template page-template-home-page page-template-home-page-php singular two-column left-sidebar">
+<div id="page" class="hfeed">
+	<header id="branding" role="banner">
+			<hgroup>
+				<h1 id="site-title"><span><a href="http://vwts.ca/" title="Transition House" rel="home">Transition House</a></span></h1>
+				<h2 id="site-description">Vernon Women&#039;s Transition House Society</h2>
+			</hgroup>
+            
+            <a href="http://ca.msn.com/" class="hide-page">hide this page</a>
+
+			
+			
+
+			<nav id="access" role="navigation">
+				<h3 class="assistive-text">Main menu</h3>
+								<div class="skip-link"><a class="assistive-text" href="#content" title="Skip to primary content">Skip to primary content</a></div>
+				<div class="skip-link"><a class="assistive-text" href="#secondary" title="Skip to secondary content">Skip to secondary content</a></div>
+								<div class="menu-navigation-container"><ul id="menu-navigation" class="menu"><li id="menu-item-194" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-103 current_page_item menu-item-194"><a href="http://vwts.ca/">Home</a></li>
+<li id="menu-item-167" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-167"><a href="http://vwts.ca/about-us/">About Us</a>
+<ul class="sub-menu">
+	<li id="menu-item-170" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-170"><a href="http://vwts.ca/about-us/publications/">Publications</a></li>
+	<li id="menu-item-169" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-169"><a href="http://vwts.ca/about-us/news-events/">News &#038; Events</a></li>
+	<li id="menu-item-610" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-610"><a href="http://vwts.ca/about-us/board/">Board</a></li>
+	<li id="menu-item-1522" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1522"><a href="http://vwts.ca/about-us/career-opportunities/">Career Opportunities</a></li>
+</ul>
+</li>
+<li id="menu-item-166" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-166"><a href="http://vwts.ca/programs-services/">Programs &#038; Services</a>
+<ul class="sub-menu">
+	<li id="menu-item-415" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-415"><a>Shelter &#038; Support Programs</a>
+	<ul class="sub-menu">
+		<li id="menu-item-176" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-176"><a href="http://vwts.ca/programs-services/shelter-support-programs/transition-house/">Transition House</a></li>
+	</ul>
+</li>
+	<li id="menu-item-416" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-416"><a>Women�s Safety, Counselling &#038; Support</a>
+	<ul class="sub-menu">
+		<li id="menu-item-177" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-177"><a href="http://vwts.ca/programs-services/womens-safety-counselling-support/womens-counselling-armstrong-enderby/">Women�s Counselling: Armstrong/ Enderby</a></li>
+		<li id="menu-item-178" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-178"><a href="http://vwts.ca/programs-services/womens-safety-counselling-support/womens-counselling-vernon/">Women�s Counselling: Vernon</a></li>
+		<li id="menu-item-179" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-179"><a href="http://vwts.ca/programs-services/womens-safety-counselling-support/womens-outreach-services/">Women�s Outreach Services</a></li>
+		<li id="menu-item-180" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-180"><a href="http://vwts.ca/programs-services/womens-safety-counselling-support/specialized-victim-assistance/">Specialized Victim Assistance</a></li>
+		<li id="menu-item-181" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-181"><a href="http://vwts.ca/programs-services/womens-safety-counselling-support/support-to-young-parents/">Support to Young Parents</a></li>
+		<li id="menu-item-182" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-182"><a href="http://vwts.ca/programs-services/womens-safety-counselling-support/integrated-case-assessment-team/">Integrated Case Assessment Team</a></li>
+		<li id="menu-item-183" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-183"><a href="http://vwts.ca/programs-services/womens-safety-counselling-support/sexual-assault-services/">Sexual Assault Services</a></li>
+		<li id="menu-item-184" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-184"><a href="http://vwts.ca/programs-services/womens-safety-counselling-support/equine-therapy/">Equine Therapy</a></li>
+		<li id="menu-item-185" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-185"><a href="http://vwts.ca/programs-services/womens-safety-counselling-support/groups-counselling/">Group Counselling</a></li>
+	</ul>
+</li>
+	<li id="menu-item-417" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-417"><a>Children�s Programs</a>
+	<ul class="sub-menu">
+		<li id="menu-item-187" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-187"><a href="http://vwts.ca/programs-services/childrens-programs/children-who-witness-abuse-counselling-vernon/">Children Who Witness Abuse Counselling</a></li>
+		<li id="menu-item-1568" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1568"><a href="http://vwts.ca/programs-services/childrens-programs/1558-2/">Oak Centre &#8211; Child and Youth Advocacy</a></li>
+	</ul>
+</li>
+	<li id="menu-item-418" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-418"><a>Justice &#038; Advocacy Programs</a>
+	<ul class="sub-menu">
+		<li id="menu-item-191" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-191"><a href="http://vwts.ca/programs-services/justice-advocacy-programs/legal-advocacy-program/">Legal Advocacy Program</a></li>
+		<li id="menu-item-192" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-192"><a href="http://vwts.ca/programs-services/justice-advocacy-programs/legal-services-society-outreach/">Legal Services Society Outreach</a></li>
+		<li id="menu-item-190" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-190"><a href="http://vwts.ca/programs-services/justice-advocacy-programs/law-clinic/">Law Clinic</a></li>
+	</ul>
+</li>
+</ul>
+</li>
+<li id="menu-item-165" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-165"><a href="http://vwts.ca/safe-shelter/">Safe Shelter</a></li>
+<li id="menu-item-164" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-164"><a href="http://vwts.ca/volunteer/">Volunteer</a></li>
+<li id="menu-item-163" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-163"><a href="http://vwts.ca/links/">Links</a></li>
+<li id="menu-item-1374" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1374"><a href="http://vwts.ca/contact/">Contact Us</a></li>
+</ul></div>			</nav><!-- #access -->
+	</header><!-- #branding -->
+
+
+	<div id="main">
+
+		<div id="primary">
+			<div id="content" role="main">
+
+				
+					
+<article id="post-103" class="post-103 page type-page status-publish hentry">
+	<!-- .entry-header -->
+
+	<div class="entry-content">
+		<!-- Start vSlider options -->
+<script type="text/javascript">
+var $jq = jQuery.noConflict(); $jq(document).ready(function() { 
+    $jq('#homeslider').coinslider({ width: 990,     height: 310, spw: 7, sph: 5, delay: 3000, sDelay: 1000, opacity: 0.7, titleSpeed: 1500, effect: 'fade', navigation: false, links : false, stickynav: false, hoverPause: true }); }); 
+    </script>
+<style type="text/css" media="screen">
+#homeslidercontainer {
+    margin: 0;
+    float:none;
+    }
+#homeslider { 
+    width: 990px; 
+    height: 310px;
+    overflow: hidden; 
+    position: relative; 
+    }
+    
+
+    #homeslider a, #homeslider a img {
+        border: none !important; 
+        text-decoration: none !important; 
+        outline: none !important;
+        } 
+        
+    #homeslider h4 {
+        color: #FFFFFF !important;
+        margin: 0px !important;padding: 0px !important;
+        font-family: Arial, Helvetica, sans-serif !important;
+        font-size: 16px !important;}
+        
+    #homeslider .cs-title {
+        background: #892d2b;
+        color: #FFFFFF  !important;
+        font-family: Arial, Helvetica, sans-serif !important;
+        font-size: 12px !important;
+        letter-spacing: normal !important;line-height: normal !important;}
+        
+    #homeslider .cs-title{ position:absolute;
+    width: 980px; padding: 10px;        }
+    #cs-buttons-homeslider { display: none; }    #homeslidercontainer .cs-buttons {clear:both; font-size: 0px; margin: 10px 0 10px 100px; float: left; }
+       #cs-button-homeslider{ z-index:999;outline:none;}
+                #homeslidercontainer .cs-buttons { font-size: 0px; padding: 10px; float: left; outline: none !important;}
+           #homeslidercontainer .cs-buttons a { margin-left: 5px; height: 15px; width: 15px; float: left; 
+                            background: url('http://vwts.ca/wp-content/plugins/vslider/images/default_style.png') no-repeat;background-position:top;
+                                                text-indent: -1000px;
+                                                outline: none !important;
+                             }
+             #homeslidercontainer .cs-buttons a:hover  { background: url('http://vwts.ca/wp-content/plugins/vslider/images/default_style.png') no-repeat;background-position: bottom;top:15px;outline: none !important;}
+            #homeslidercontainer  a.cs-active { background: url('http://vwts.ca/wp-content/plugins/vslider/images/default_style.png') no-repeat;background-position:bottom;outline: none !important;}          
+                                
+        
+                 #homeslider  .cs-prev,#homeslider  .cs-next { outline:none; }
+                #homeslider  .cs-prev,#homeslider .cs-next {font-weight: bold;background: #892d2b !important;font-size: 28px !important;font-family: "Courier New", Courier, monospace;color: #FFFFFF 
+!important;padding: 0px 10px !important;-moz-border-radius: 5px;-khtml-border-radius: 5px;-webkit-border-radius: 5px;}
+         
+       #homeslider,#homeslider img {
+        border:0px solid #FFFFFF; 
+        border-radius:0px;
+        -moz-border-radius:0px;
+        -webkit-border-radius:0px;
+        }
+</style>
+<!-- End vSlider options -->
+    <div id="homeslidercontainer">
+    <div id="homeslider">       <a href="" style="background:#fff;" target="">
+              <img src="http://vwts.ca/wp-content/uploads/2012/11/img-slider2.jpg" style="width:990px;height:310px;" alt="" />
+                </a>
+    </div></div>
+			</div><!-- .entry-content -->
+	<!-- .entry-meta -->
+</article><!-- #post-103 -->
+
+					
+				
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+
+	</div><!-- #main -->
+
+	<footer id="colophon" role="contentinfo">
+
+			<div class="footer-graphic">Transition House: 250-542-1122 (24/7) If you are in immediate danger please dial 911.</div>
+            
+            			
+			
+<div id="supplementary" class="three">
+		<div id="first" class="widget-area" role="complementary">
+		<aside id="miniloops-2" class="widget miniloops"><h3 class="widget-title"></h3><ul><li><h2><a href="http://vwts.ca/2016/10/nixon-wenger-presents-4th-annual-little-black-dress-event/">Nixon Wenger Presents 4th Annual Little Black Dress Event</a></h2><p> <a href="http://vwts.ca/2016/10/nixon-wenger-presents-4th-annual-little-black-dress-event/">MORE NEWS &amp; EVENTS &gt;</a></p></li></ul></aside>	</div><!-- #first .widget-area -->
+	
+		<div id="second" class="widget-area" role="complementary">
+		<aside id="text-2" class="widget widget_text">			<div class="textwidget"><div class="ftr-buttons">
+<a class="donate" title="Donate Now" href="http://www.canadahelps.org/CharityProfilePage.aspx?CharityID=s12154" target="_blank">Donate Now</a>
+<a href="wish-list/" class="wishlist">Our Wish List</a>
+</div></div>
+		</aside>	</div><!-- #second .widget-area -->
+	
+		<div id="third" class="widget-area" role="complementary">
+		<aside id="text-3" class="widget widget_text">			<div class="textwidget">Funded by the Province of BC</div>
+		</aside>	</div><!-- #third .widget-area -->
+	</div><!-- #supplementary -->
+				</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<script type='text/javascript' src='http://vwts.ca/wp-includes/js/comment-reply.min.js?ver=4.2.11'></script>
+<script type='text/javascript' src='http://vwts.ca/wp-includes/js/jquery/ui/widget.min.js?ver=1.11.4'></script>
+<script type='text/javascript' src='http://vwts.ca/wp-includes/js/jquery/ui/effect.min.js?ver=1.11.4'></script>
+<script type='text/javascript' src='http://vwts.ca/wp-includes/js/jquery/ui/effect-shake.min.js?ver=1.11.4'></script>
+
+</body>
+</html>
